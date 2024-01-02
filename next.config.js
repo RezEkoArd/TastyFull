cea690d71da6425d7849ec48ff99deb896ee3417
Rezekoard <rezkyardianto1222@gmail.com>
2024-01-02 08:01:50 +0700

    updated video bg

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    images: {
-      loader: 'custom',
-      loaderFile: '@/app/libs/imageLoader',
-    },
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     images: {
+//       loader: 'custom',
+//       loaderFile: '@/app/libs/imageLoader',
+//     },
   
-  },
+//   },
 
  
+// }
+
+// module.exports = nextConfig
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
 }
-
+ 
 module.exports = nextConfig
-
-const withVideos = require('next-videos')
-
-module.exports = withVideos()
-
--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from "react";
 import styles from "./page.module.css";
-import Link from 'next/link';
-import video from "@/videos/video.mp4"
+import Link from "next/link";
+// import videoBg from "@/assets/videoBg.mp4";
+
 const Hero = () => {
   return (
     <div className={styles.contents}>
-        <div className={styles.title}>
-            <h1>Tastyfull</h1>
-            <p>New Experience Journey your Tastyful Bites</p>
-            <div className={styles.buttons}>
-                <Link href={"#first-section"}  className='text-color-primary 
-                p-[1rem] border-2 border-color-primary hover:scale-105 '>See Recommended Restaurant</Link>
-            </div>
+      <div className={styles.title}>
+        <h1>Tastyfull</h1>
+        <p>New Experience Journey your Tastyful Bites</p>
+        <div className={styles.buttons}>
+          <Link
+            href={"#first-section"}
+            className="text-color-primary 
+                p-[1rem] border-2 border-color-primary hover:scale-105 "
+          >
+            See Recommended Restaurant
+          </Link>
         </div>
-        <video 
-        src={video} 
-        autoPlay 
-        muted 
-        loop 
-        className={styles.video}
-      />
+      </div>
+      <video autoPlay loop muted className={styles.video}>
+        <source src="/videoBg.mp4" type="video/mp4"/>
+      </video>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
